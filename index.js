43aeabd2d59eea3323b49e273df1a1a20d745765
starkweb108 <starkweb2@gmail.com>
2023-03-19 23:21:19 +0100

    made a correction

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const errorHandlerMiddleware = require('./middlewares/errorHandler');
 
 // middleware
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
