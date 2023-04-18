158441cb277b613d0764ad22ce0321d66bf2d795
starkweb108 <starkweb2@gmail.com>
2023-04-18 11:22:18 +0100

    fix commit error

--- controllers/auth/logutUser.js
+++ controllers/auth/logutUser.js
@@ -3,15 +3,15 @@ const logoutUser = async (req, res) => {
     expires: new Date(Date.now()),
     httpOnly: true,
     signed: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'None',
+    // secure: process.env.NODE_ENV === 'production',
+    // sameSite: 'None',
   });
   res.cookie('refreshToken', '', {
     expires: new Date(Date.now()),
     httpOnly: true,
     signed: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'None',
+    // secure: process.env.NODE_ENV === 'production',
+    // sameSite: 'None',
   });
   res.status(200).json({ message: `Account logged out` });
 };
