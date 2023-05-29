0d084f8ec2464bf5b10da12449c03c8aaa61aaf7
starkweb108 <starkweb2@gmail.com>
2023-05-29 09:40:48 +0100

    fixing cookies not sent in some browser error

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 const cloudinary = require('cloudinary').v2;
 const fileUpload = require('express-fileupload');
+const cookiesMiddleware = require('universal-cookie-express');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -40,11 +41,12 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
+
 app.use(express.json());
 app.use(fileUpload({ useTempFiles: true, tempFileDir: '/tmp/' }));
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(morgan('tiny'));
-
+app.use(cookiesMiddleware());
 app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/user', userRoutes);
